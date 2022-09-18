4be36f15756a5edf30921931b6e29bfd3fa5e3da
MaxSpass <maxspas1@gmail.com>
2022-09-18 10:55:49 +0200

    Completed: focus window

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import os
-import cv2
+import pyautogui
+from classes.window_mgr import *
+
+GAME_WINDOW = 'Raid: Shadow Legends'
+
+
+def prepare_window():
+    w = WindowMgr()
+    w.find_window_wildcard(".*%s*" % GAME_WINDOW)
+    w.adjust_window()
+    w.set_foreground()
+
+def main():
+    pyautogui.FAILSAFE = True
+
+    prepare_window()
+
+
+if __name__ == "__main__":
+    main()
