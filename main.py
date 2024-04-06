56d065e26dd7855b8d2404b2019ed99e6516ba5a
MaxSpass <maxspas1@gmail.com>
2024-04-06 19:59:06 +0200

    Reworked main.py and changed a bot config.json. Removed unnecessary functionality from in_progress.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import threading
 import traceback
-
 import pyautogui
 from bot import TelegramBOT
-from classes.app import *
+from classes.App import *
 from constants.index import IS_DEV
 
 # from telegram.ext import CommandHandler
@@ -18,7 +16,6 @@ from constants.index import IS_DEV
 # from features.faction_wars.index import *
 # from features.hero_preset.index import HeroPreset
 # from features.doom_tower.index import *
-# from classes.task_iterator import TaskIterator
 # from features.quests.index import *
 # from in_progress import *
 
@@ -35,47 +32,6 @@ else:
 
 def main():
     app = App()
-
-    # def test(*args):
-    #     msg = 'Test'
-    #     counter = 0
-    #     while counter < 3:
-    #         print(f'{msg}: {counter}')
-    #         counter += 1
-    #         sleep(1)
-    # def screen_test():
-    #     screenshot = pyautogui.screenshot(region=[0, 0, 500, 500])
-    #
-    #     # Convert the screenshot to bytes
-    #     image_bytes = BytesIO()
-    #     screenshot.save(image_bytes, format='PNG')
-    #     image_bytes.seek(0)
-    #
-    #     return image_bytes
-    # telegram_bot = TelegramBOT({
-    #     'token': app.config['telegram_token']
-    # })
-    # telegram_bot.start()
-    # telegram_bot.add({
-    #     'command': 'screen',
-    #     'description': 'Capture and send a screenshot',
-    #     'handler': {
-    #         'type': 'async',
-    #         'callback': lambda upd, ctx: ctx.bot.send_photo(
-    #             chat_id=upd.message.chat_id,
-    #             photo=screen_test()
-    #         ),
-    #     },
-    # })
-    # telegram_bot.add({
-    #     'command': 'test',
-    #     'handler': {
-    #         'callback': lambda upd, ctx: app.queue.put(test)
-    #     }
-    # })
-    # telegram_bot.listen()
-    # telegram_bot.join()
-    # return
 
     if IS_DEV or app.validation():
         game_path = app.get_game_path()
@@ -100,45 +56,41 @@ def main():
                     telegram_bot.add({
                         'command': 'restart',
                         'description': 'Re-Start the Game',
-                        'handler': {
-                            'callback': lambda upd, ctx: app.queue.put(app.restart),
-                        },
+                        'handler': app.task(name='restart', cb=app.restart),
                     })
                     telegram_bot.add({
                         'command': 'launch',
                         'description': 'Re-Launch the Game',
-                        'handler': {
-                            'callback': lambda upd, ctx: app.queue.put(app.launch),
-                        },
+                        'handler': app.task(name='launch', cb=app.launch),
                     })
                 telegram_bot.add({
                     'command': 'relogin',
                     'description': 'Re-log in',
-                    'handler': {
-                        'callback': lambda upd, ctx: app.queue.put(app.relogin),
-                    },
+                    'handler': app.task(name='relogin', cb=app.relogin),
                 })
 
-                # Async
+                # Sync
                 telegram_bot.add({
                     'command': 'report',
                     'description': 'Report',
-                    'handler': {
-                        'type': 'async',
-                        'callback': lambda upd, ctx: app.report(),
-                    },
+                    'handler': app.task(
+                        name='report',
+                        cb=app.report,
+                        task_type='sync'
+                    ),
                 })
-                # Async
+                # Sync
                 telegram_bot.add({
                     'command': 'screen',
                     'description': 'Capture and send a screenshot',
-                    'handler': {
-                        'type': 'async',
-                        'callback': lambda upd, ctx: ctx.bot.send_photo(
+                    'handler': app.task(
+                        name='screen',
+                        cb=lambda upd, ctx: ctx.bot.send_photo(
                             chat_id=upd.message.chat_id,
                             photo=app.screen()
                         ) if bool(app.window) else upd.message.reply_text("No Game window found"),
-                    },
+                        task_type='sync'
+                    ),
                 })
 
                 # register main commands according to 'tasks'
@@ -147,11 +99,10 @@ def main():
                     regular_command = list(map(lambda task: {
                         'command': task['command'],
                         'description': f"command '{task['title']}'",
-                        'handler': {
-                            'callback': lambda upd, ctx: app.queue.put(app.get_entry(
-                                command_name=task['command']
-                            )['instance'].run),
-                        },
+                        'handler': app.task(
+                            name=task['command'],
+                            cb=app.get_entry(command_name=task['command'])['instance'].run
+                        ),
                     }, app.config['tasks']))
 
                 # register addition commands according to 'presets'
@@ -160,11 +111,12 @@ def main():
                     presets_commands = list(map(lambda preset: {
                         'command': make_command_key(f"preset {preset['name']}"),
                         'description': f"commands in a row: {', '.join(preset['commands'])}",
-                        'handler': {
-                            'callback': lambda upd, ctx: app.queue.put(lambda: list(map(lambda x: app.get_entry(
+                        'handler': app.task(
+                            name=make_command_key(f"preset {preset['name']}"),
+                            cb=lambda: list(map(lambda x: app.get_entry(
                                 command_name=x
-                            )['instance'].run(), preset['commands']))),
-                        },
+                            )['instance'].run(), preset['commands']))
+                        ),
                     }, app.config['presets']))
 
                 commands = regular_command + presets_commands
@@ -181,8 +133,6 @@ def main():
         except Exception:
             error = traceback.format_exc()
             log_save(error)
-        finally:
-            log('All tasks are done')
 
             if has_telegram_token and telegram_bot:
                 # Wait for the bot thread to finish
